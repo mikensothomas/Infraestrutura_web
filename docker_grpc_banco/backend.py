77f5ce36b678307ef53a936f1f773e8cb48b9597
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-06-22 22:19:06 -0300

    Commit

--- docker_grpc_banco/backend.py
+++ docker_grpc_banco/backend.py
@@ -4,21 +4,17 @@ import messenger_pb2
 import messenger_pb2_grpc
 import psycopg2
 
-# Configuração do banco de dados PostgreSQL
-DB_HOST = 'banco'  # Nome do serviço do contêiner do banco de dados no Docker Compose
-DB_PORT = '5432'    # Porta padrão do PostgreSQL
-DB_NAME = 'banco_grpc' # Nome do banco de dados
-DB_USER = 'usuario' # Usuário do banco de dados
-DB_PASSWORD = '12345' # Senha do banco de dados
+DB_HOST = 'banco'
+DB_PORT = '5432'
+DB_NAME = 'banco_grpc'
+DB_USER = 'usuario'
+DB_PASSWORD = '12345'
 
-# Classe que implementa o serviço gRPC Messenger
 class MessengerServicer(messenger_pb2_grpc.MessengerServicer):
     def SendMessage(self, request, context):
-        # Obtém os dados da mensagem do cliente
         sender = request.sender
         content = request.content
                 
-        # Conecta ao banco de dados PostgreSQL
         try:
             conn = psycopg2.connect(
                 host=DB_HOST,
@@ -33,13 +29,11 @@ class MessengerServicer(messenger_pb2_grpc.MessengerServicer):
             return messenger_pb2.Empty()
         
         try:
-            # Cria um cursor para executar operações SQL
             with conn.cursor() as cursor:
                 print("\n")
                 print("==============++++++++++++++++++++++=============")
                 print("\n")
                 print(f"Mensagem recebida do cliente {sender}: {content}.")
-                # Insere a mensagem na tabela messages
                 sql = "INSERT INTO mensagem (nome, mensagens) VALUES (%s, %s)"
                 cursor.execute(sql, (sender, content))
                 conn.commit()
@@ -53,10 +47,8 @@ class MessengerServicer(messenger_pb2_grpc.MessengerServicer):
             conn.close()
             print("==============++++++++++++++++++++++=============")
         
-        # Retorna uma resposta vazia para o cliente
         return messenger_pb2.Empty()
 
-# Função para iniciar o servidor gRPC
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     messenger_pb2_grpc.add_MessengerServicer_to_server(MessengerServicer(), server)
